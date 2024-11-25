605ffd40fe13d9a174e9ec7fa622e20f963ab102
DHRUMIL PATEL <dpatel24@radar.gsw.edu>
2024-11-25 13:00:43 -0500

    .

--- server/jwt_key.py
+++ server/jwt_key.py
@@ -4,5 +4,13 @@
 import secrets
 
 """ Step 2: Generate a secret key """
+JWT_key = secrets.token_urlsafe(32)
+print(JWT_key)
+
+# Generate a 32-byte secret key
 secret_key = secrets.token_urlsafe(32)
-print(secret_key)
+print(f"SECRET_KEY={secret_key}")
+
+# Generate a 16-byte security password salt
+security_password_salt = secrets.token_urlsafe(16)
+print(f"SECURITY_PASSWORD_SALT={security_password_salt}")
